787b39b103bbf75b544f52808f16f16346e0e8f4
davidemioma <davidemiomauche@gmail.com>
2021-08-18 15:42:07 +0100

    room_homepage

---
+++ main.js
@@ -0,0 +1,15 @@
+function changeBg(){
+  const images = [
+    'url(./images/desktop-image-hero-1.jpg)',
+    'url(./images/desktop-image-hero-2.jpg)',
+    'url(./images/desktop-image-hero-3.jpg)'
+  ]
+
+  const section = document.querySelector('header');
+  const bg = images[Math.floor(Math.random() * images.length)];
+  section.style.background = bg;
+  section.style.backgroundRepeat = 'no-repeat';
+  section.style.backgroundSize = 'cover';
+}
+
+setInterval(changeBg, 10000);
